e28b65d39916517a9b96bb61c6a824d68d2ce934
Prasun <prasun_1993@rediffmail.com>
2016-10-06 15:09:41 +0530

    [Prasun] exporting processed data by papaparse

--- process_data.js
+++ process_data.js
@@ -2,4 +2,5 @@ var papa = require('papaparse');
 var fs = require('fs');
 var text = fs.readFileSync('data/mortality_data.csv','utf8');
 var parsedData = papa.parse(text);
-console.log(parsedData.data);
+
+module.exports = {parsedData:parsedData};
