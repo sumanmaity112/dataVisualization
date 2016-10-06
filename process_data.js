c76ba828749ebd7f352a1449516f8e10d1e89b55
Supriya Gole <supriyag@thoughtworks.com>
2016-10-06 14:20:32 +0530

    Supriya/Suman : Added mortality data and written sample code to parse CSV using papaparse.

--- process_data.js
+++ process_data.js
@@ -1,6 +1,34 @@
 var papa = require('papaparse');
 var fs = require('fs');
-var text = fs.readFileSync('data/mortality_data.csv','utf8');
-var parsedData = papa.parse(text).data;
 
-module.exports = {parsedData:parsedData};
+var processData = function () {
+    var text = fs.readFileSync('data/mortality_data.csv', 'utf8');
+    var parsedData = papa.parse(text).data;
+    var headers = parsedData[0];
+    headers.shift();
+    headers.pop();
+    parsedData.shift();
+    return convert(headers, parsedData);
+};
+
+var getCountryNames = function (data) {
+    return data.map(function (entry) {
+        return entry[0];
+    });
+};
+
+var convert = function (headers, data) {
+    var mortalityCountryWiseData = {};
+    var countries = getCountryNames(data);
+
+    countries.forEach(function (country, index) {
+        mortalityCountryWiseData[country] = {};
+        var information = data[index];
+        headers.forEach(function (header, index) {
+            mortalityCountryWiseData[country][header] = information[++index];
+        });
+    });
+    return mortalityCountryWiseData;
+};
+
+module.exports = processData;
